4ae5978b5636530fc2a91c5fd08d3b211f7b9843
Jeffrey Platt <jsplatt@gmail.com>
2020-11-30 18:05:58 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,1 +1,9 @@
-def find
+def find_element_index(array, value_to_find)
+  count = 0
+  while count < value_to_find do
+    if array[count] == value_to_find
+      return count
+    end
+    count += 1
+  end
+end  
